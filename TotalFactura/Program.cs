e73ef1ca4b8c30307fd00b874c29e7f6e0a0a870
aleastudillo <50086026+aleastudillo@users.noreply.github.com>
2019-04-28 18:14:57 -0500

    Add files via upload

---
+++ TotalFactura/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TotalFactura
+{
+    class Program
+    {
+        //Crear un programa que lea cantidades y precios y al final indique el total de la factura.
+            //Primero se pregunta: 
+                //Introduzca la cantidad vendida tras lo cual el usuario introducirá un número entero positivo.
+            //Después se pregunta:
+                //Introduzca el precio que será un número decimal positivo.
+                //La lectura termina cuando en la cantidad se introduzca un cero.Si es así se escribirá el total.
+
+        static void Main(string[] args)
+        {
+            int entero = 0;
+            double precio = 0, total=0;
+            do
+            {
+                do
+                {
+                    Console.WriteLine("Ingrese la cantidad vendida: ");
+                    entero = Convert.ToInt16(Console.ReadLine());
+                    if (entero < 0)
+                    {
+                        Console.Write("Cantidad no valida");
+                    }
+                } while (entero < 0);
+                if (entero > 0)
+                {
+                    Console.WriteLine("Ingrese el precio: ");
+                    do
+                    {
+                        precio = Convert.ToDouble(Console.ReadLine());
+                        if (precio < 0)
+                        {
+                            Console.Write("Precio no valido");
+                        }
+                        else
+                        {
+                            total += entero * precio;
+                        }
+                    } while (precio < 0);
+                }
+            } while (entero != 0);
+
+            Console.WriteLine("El Total vendido es: " + total);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ UnoDosTres/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnoDosTres
+{
+    class Program
+    {
+        //Crear un programa en C# que lea un número entero y positivo y que escriba tres columnas. 
+        //La primera cuenta desde uno hasta el número escrito contando de uno en uno; la segunda columna 
+        //cuenta de dos en dos y la tercera de tres.
+
+        static void Main(string[] args)
+        {
+            int i1 = 0, i2 = 0;
+            Console.WriteLine("Ingrese un la cantidad de numeros de la serie: ");
+            int entero = Convert.ToInt16(Console.ReadLine());
+            for (int i = 0; i <= entero; i++)
+            {
+                i1 = ((i + 2) + i);
+                i2 = ((i +3) + i +i);
+                Console.WriteLine((i) + "  " + "  " + "  " + (i1) + "  " + "  " + "  " +(i2));
+            }
+            Console.ReadKey();
+        }
+    }
+}
