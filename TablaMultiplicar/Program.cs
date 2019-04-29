295fddd1f3b0bef10219ba711db1dc86a209cca6
aleastudillo <50086026+aleastudillo@users.noreply.github.com>
2019-04-29 00:20:43 -0500

    Add files via upload

---
+++ TablaMultiplicar/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TablaMultiplicar
+{
+    class Program
+    {
+        //Crear el programa tablaMultiplicar que escriba la tabla de multiplicar del número 1 al número 15
+        static void Main(string[] args)
+        {
+            int numero=0 , multiplicacion = 0;
+
+            Console.WriteLine("Multiplicacion: ");
+            numero = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= 15; i++)
+            {
+                multiplicacion = numero * i;
+                Console.WriteLine( i + "*" + numero + "=" + multiplicacion);
+            }
+            Console.ReadKey();
+        }
+    }
+}
